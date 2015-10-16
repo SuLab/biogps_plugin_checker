93936b9d20800740efc020f3bd062fb635fc4699
Julee  Adesara <jadesara@sulab.scripps.edu>
2015-10-16 10:00:37 -0700

    Modified file to create html table

--- biogps_plugin_checker.py
+++ biogps_plugin_checker.py
@@ -1,40 +1,15 @@
-import urllib2
-import json
-import csv
-import httplib2
+import urllib2      #To open url
+import json         #To load json data
+import httplib2     #To get status code
 
 human_gene_id_list = [1017,7157,695,3702,5599,3558,1743,65220,132,3485]
 mouse_gene_id_list = [12566,12229,16428,26419,16183,78920,192185,11534,16008]
 rat_gene_id_list = [362817,24842,367901,363577,116554,116562,299201,100125370,25368,25662]
 url = "http://biogps.org/plugin/all/?format=json&limit=381"
 
-
-# Function 1 - should open the input url, parse it to prepare url_list
-# input  = url
-# output = url_list
-
-'''def ExtractUrl(url,gene_id_list):
-    
-    url_list = []
-    req = urllib2.Request(url)
-    opener = urllib2.build_opener()
-    f = opener.open(req)
-    json1 = json.load(f)
-    
-    for j in gene_id_list:
-        for item in json1:
-            url1 = item.get('url')
-            url_list.append(url1)
-    return url_list
-'''
-
-# Function 2 - should open the input url, parse it to plugin_id_list
-# input = url
-# output = plugin_id_list
-
-def ExtractPluginId(url):
-    plugin_id_list = []
-    
+#Function 1: Give plugin_id list
+def ExtractPluginId(url):     
+    plugin_id_list = []    
     req = urllib2.Request(url)
     opener = urllib2.build_opener()
     f = opener.open(req)
@@ -42,116 +17,131 @@ def ExtractPluginId(url):
     
     for item in json1:
         id1 = item.get('id')
-        plugin_id_list.append(id1)
-    #print "PLUGIN_IDS' : ", plugin_id_list
+        plugin_id_list.append(id1)   
     return plugin_id_list
 
     
-# Function 3 - should create new_url_list using input plugin_id_list and human_gene_id_list
-# input = plugin_id_list
-# input = human_gene_id_list
-# output = new_url_list
-
-def UrlList(plugin_id_list,gene_id_list):
-    
-    new_url_list = []
-    for x in gene_id_list:
-        for y in plugin_id_list:
-            new_url = "http://biogps.org/plugin/"+str(y)+"/renderurl/?geneid="+str(x)
-            new_url_list.append(new_url)
-    return new_url_list
-
-
-# Function 4
-# input  new_url_list
-# output status_code_list or a dictonary whose key=new_url_list elements and value=status_code
-
-def StatusCode(new_url_list):
-    dictionary = dict()
-    l1 = []
-    
-    #sock=socket.socket()
-    #sock.settimeout(60)
-    for i in new_url_list:
-        #print "key: ",i
-        data = urllib2.urlopen(i)
-        for lines in data:
-            data1 = json.loads(lines)            
-            #print "EXTRACTED_URL: ",data2          
-            try:
-                data2 = data1['url']
-                h = httplib2.Http(timeout=30)
-                resp,content = h.request(data2)
-                d1 = resp.status
-                #print i, d1
-                #print "STATUS_CODE: ",d1
-                dictionary[i] = d1
-                #print dictionary
-                
-            except:
-                dictionary[i] = "error"
-                #print "Error"
-                    #print dictionary
-                        
-    return dictionary
+#Function 2:Get status code of url
+def getCode(main_url):
+    data = urllib2.urlopen(main_url)
+    for lines in data:           
+        data1 = json.loads(lines)                    
+        try:
+            data2 = data1['url']    
+            h = httplib2.Http(timeout=30)
+            resp,content = h.request(data2)
+            d1 = resp.status           
+        except:
+            d1 = "Error"
+    return d1
 
 
-
-# Function 5
-# - declare inputs 
-# - Call function 1 and get url_list
-# - call function 2 and get plugin_id_list
-# - call function 3 to get new_url_list
-# - call function 4 to get dictonary (where key = new_url_list (not the url from which we get status code), value = statuscode
-# - Create a CSV/html file either using dictory or (url_list + plugin_id_list)
-
-def StatusCodeInCsv(url,gene_id_list):
-    # Open text file and add header
-    f = open("myfile_mouse_genes.txt",'w')
-    f.write("\t")    
-    for m in gene_id_list:
-        string = str(m) + "\t"
-        f.write(str(string))
-    f.write("\n")
-    #print " ----------- Created header in text file -----------"
-
-    # Create dictionary whose key=URL and value is status code
-    plugin_id_list = []
-    urllist = []
-    dictionary = dict()
+#Function 3: Give html table of status code with sorting feature
+def get_html_table(url,gene_id_list):
+    #local variables
+    html = []
+    plugin_id_list = []    
     plugin_id_list = ExtractPluginId(url)
-    #print " ----------- Extracted plugin id from url ---------- "
-    urllist = UrlList(plugin_id_list,gene_id_list)
-    #print " ----------- Created URL list to find out status code -------- "
-    dictionary = StatusCode(urllist)
-    #print " ----------- Created dictionary for URL and Status code ------- "
+    
+    # Create a head and provide jquery tablesorter plugin
+    html1 = """
+    <head>
+    <script src="http://tablesorter.com/jquery-latest.js" type="text/javascript"></script>
+    <script src="http://tablesorter.com/__jquery.tablesorter.min.js" type="text/javascript"></script>
+    <script type="text/javascript"></script>   
+    
+    <script>
+        $(document).ready(function(){
+        $(function(){
+        $("#Biogps_plugins").tablesorter();
+        });
+        });
+    </script>
+    
+    <style>
+    p{
+    display: block;
+    align: bottom;
+    }    
+    caption{    
+    display: table-caption;
+    text-align: center;
+    font-size: 30;
+    font-style: italic;
+    color: #CD5C5C;
+    }
+    table {
+    border-radius: 10px;
+    border: 2px solid #FFDAB9;
+    padding: 5px;
+    width: 100px;
+    height: 100px;
+    }
+    td {    
+    background-color: #F5F5DC;
+    padding: 15px;
+    height: 15px;
+    vertical-align: bottom;
+    }
+    th {    
+    background-color: #FFDAB9;    
+    height: 15px;
+    padding: 15px;
+    }    
+    </style>
+    
+    </head>
+    <body>
+    <table id="Biogps_plugins" class="tablesorter" border="2" cellspacing="5"cellpadding="12">
+    <caption>Biogps Plugins Checker</caption>
+    <thead>
+    """
+    html.append(html1)    
+   
+    # Create column for gene_id_list, keep first column void to match rows indentation.
+    headerlist = list(gene_id_list)
+    headerlist.insert(0,"geneid/pluginid")
+    if headerlist:
+        html.append("<tr>")
+        for header in headerlist:
+            html.append("<th>%s</th>" % header)
+        html.append("</tr>")
+    html.append("</thead>")
 
-    # Create a table in text file    
+    # Create body
+    html.append("<tbody>")
+    
+    # Scan and generate status code (row=pluginid, col=geneid)
     for j in plugin_id_list:
-        string = str(j)+"\t"
-        f.write(str(string))
-        col=1;
+        html.append("<tr>")
+        html.append('<td>%s</td>'%j)     
         for i in gene_id_list:
-            mykey = "http://biogps.org/plugin/"+str(j)+"/renderurl/?geneid="+str(i)
-            string = str(dictionary[mykey])+"\t"
-            f.write(str(string))
-        f.write("\n")
-    f.close()
+            main_url = "http://biogps.org/plugin/"+str(j)+"/renderurl/?geneid="+str(i)
+            sub_url  = main_url+"&redirect=true"
+            status = getCode(main_url)
+            html.append('<td><a href=%s>%s</a>'%(sub_url,status))
+            html.append('</td>')
+        html.append("</tr>")
+    html.append("</tbody")
+    html2 = '''
+    <p>
+    Notes:<br>
+    First row = gene-ids<br>
+    First column = plugin-ids<br>
+    Each cell represents status code of related gene-id and plugin-id.<br>
+    Click anywhere in the First row for sorting. <br>   
+    Click on any cell to open corresponding url.<br>    
+    </p>
+    '''
+    
+    html.append(html2)    
+    html.append("</table")     
+    html.append('</body>')
+    html_table=  "\n".join(html)    
+    html_file = open("output_human_gene_id.html","w")
+    html_file.write(html_table)
+    html_file.close()
+    return html_file
+print get_html_table(url,human_gene_id_list)
 
-    #Generate csv from text file
-    c=open("output_mouse_genes.csv","wb")
-    writer = csv.writer(c)
-    f = open("myfile_mouse_genes.txt",'r')
-    data = f.readlines()
-    for line in data:
-        words = line.split("\t")
-        #print "----------"
-        #print words
-        writer.writerow(words)
-
-    f.close()
-    c.close()
-    return f
-
-print StatusCodeInCsv(url,mouse_gene_id_list)
 
